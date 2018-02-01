9b73a2895998c4821695782ad01ffea8a700dd61
ningcheng <1520274124@qq.com>
2018-02-01 10:48:46 +0800

    update

---
+++ init-common/src/main/java/com/ximalaya/init/common/web/result/Page.java
@@ -0,0 +1,45 @@
+package com.ximalaya.init.common.web.result;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.List;
+
+/**
+ * @author ningcheng
+ * @date 2018/1/31
+ */
+@ApiModel(value = "分页查询结果")
+public class Page<T> {
+
+    @ApiModelProperty(notes = "总页数")
+    private Integer totalPage;
+    @ApiModelProperty(notes = "总记录数")
+    private Integer totalCount;
+    @ApiModelProperty(notes = "结果明细")
+    private List<T> results;
+
+    public Integer getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(Integer totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public Integer getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(Integer totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public List<T> getResults() {
+        return results;
+    }
+
+    public void setResults(List<T> results) {
+        this.results = results;
+    }
+}
